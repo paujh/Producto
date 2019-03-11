e3340f127ceeb893c1cc39556d4a57add31c8118
paujh <48219253+paujh@users.noreply.github.com>
2019-03-11 12:05:27 -0600

    Update Producto.py

--- Producto.py
+++ Producto.py
@@ -3,7 +3,8 @@ March.03
 Paula Jorge Hinostrosa
 '''
 
-class Producto: 
+class Producto:
+	
   def __init__(self,nombre,marca,precio):
   #Le damos los atributos que deseemos a la clase, en este caso, tres.
   #Entonces, el constructor recibirá tres argumentos.
@@ -11,6 +12,7 @@ class Producto:
     self.marca=marca
     self.precio=precio
   def imprimirDetalles(self):
+		
 	#Este método es para que se visualicen los argumentos de la clase ya creada
     print("Desde el método:") 
     print("Nombre del producto:",self.nombre)
