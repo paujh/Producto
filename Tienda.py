694ac01154fb59d111348f7d23660d090baf416b
paujh <48219253+paujh@users.noreply.github.com>
2019-03-11 12:48:13 -0600

    Update Tienda.py

--- Tienda.py
+++ Tienda.py
@@ -4,15 +4,20 @@ Paula Jorge Hinostrosa
 '''
 
 from Producto import *
+from Cliente import *
 
 class Tienda:
   
-  def __init__(self,nombre,producto):
+  
+  def __init__(self,nombre,producto,cliente):
+    #Esta ocación, el constructo rebibirá 3 argumentos, donde 2 de ellos son objetos importados de otras clases ya creadas
     self.nombre=nombre
     self.producto=producto
+    self.cliente=cliente
   
   def imprimirDetalles(self):
     #Este método es para que se visualicen los argumentos de la clase ya creada
     print("Desde el método:")
     print("El nombre de la tienda es:",self.nombre)
     self.producto.imprimirDetalles
+    self.cliente.imprimirDetalles
