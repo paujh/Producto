6140e67f1319b0e7837ecaa3843408f0c1840bdc
paujh <48219253+paujh@users.noreply.github.com>
2019-03-11 12:16:24 -0600

    Create Tienda.py

---
+++ Tienda.py
@@ -0,0 +1,18 @@
+'''
+March.03
+Paula Jorge Hinostrosa
+'''
+
+from Producto import *
+
+class Tienda:
+  
+  def __init__(self,nombre,producto):
+    self.nombre=nombre
+    self.producto=producto
+  
+   def imprimirDetalles(self):
+    #Este método es para que se visualicen los argumentos de la clase ya creada
+    print("Desde el método:")
+    print("El nombre de la tienda es:",self.nombre)
+    self.producto.imprimirDetalles
