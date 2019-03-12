1d1237efbefc0dfbf686f0a80d716a75871debee
paujh <48219253+paujh@users.noreply.github.com>
2019-03-11 20:16:54 -0600

    Update Pruebas.py

--- Pruebas.py
+++ Pruebas.py
@@ -10,7 +10,6 @@ from Tienda import *
 class Pruebas: #Aquí indicamos que la clase Pruebas está vacía, ya que sólo queremos que sea para probar la clase Producto
 	pass
 
-print ("Imprimimos atributos desde el archivo principal(Producto)")#Indicamos al usuario que la información se tomará desde "Producto"
 
 #la cantidad de argumentos que se le pasa al constructor, cambia, en este caso son tres
 P1=Producto("Muñeco Batman","LEGO",650)
