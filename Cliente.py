11daf0a98bf5eeb8622446462cd26c2dd32a4f5b
paujh <48219253+paujh@users.noreply.github.com>
2019-03-28 20:22:25 -0600

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -7,8 +7,8 @@ class Cliente:
 
   #Esta clase recibirá 2 atributos, el nombre del cliente, y el saldo del mismo.
   def __init__(self,nombre,saldo):
-    self.nombre=nombre 
-    self.saldo=saldo
+    self.nombre = nombre 
+    self.saldo = saldo
   
   #Reemplazaremos el método imprimirDetalles por str, que hacen lo mismo, pero el último podría decrise que es más fácil de manejar al ya ser determinado del programa
   def __str__(self):
