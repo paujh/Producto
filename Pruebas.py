a012385c7b8e4bee1d87bd2792548ad9f369be35
paujh <48219253+paujh@users.noreply.github.com>
2019-04-08 23:48:16 -0500

    Update Pruebas.py

--- Pruebas.py
+++ Pruebas.py
@@ -6,18 +6,24 @@ Paula Jorge Hinostrosa
 from Producto import *
 from Cliente import *
 from Tienda import *
+from MenuCliente import*
+from Carrito import*
 
 class Pruebas: #Aquí indicamos que la clase Pruebas está vacía, ya que sólo queremos que sea para probar la clase Producto
 	pass
 
 
+#Pruebas del MenuCliente
+cliente1 = Cliente(nombre,saldo)
+print(cliente1)
+
 #la cantidad de argumentos que se le pasa al constructor, cambia, en este caso son tres
-muñeco = Producto("Muñeco Batman","LEGO",650)
+muneco = Producto("Muñeco Batman","LEGO",650)
 triciclo = Producto("Triciclo","Apache",1199)
-muñecas = Producto("Set de Muñecas Disney Collection Princesas","DISNEY COLLECTION",2295)
-print(muñeco)
+munecas = Producto("Set de Muñecas Disney Collection Princesas","DISNEY COLLECTION",2295)
+print(muneco)
 print(triciclo)
-print(muñecas)
+print(munecas)
 
 maria = Cliente("María Pérez",2500)
 margarita = Cliente("Margarita Téllez",1450)
@@ -32,3 +38,11 @@ tienda3 = Tienda("Toy Store","Parroquia 179, Col del Valle Sur, 03100 Ciudad de 
 print(tienda1)
 print(tienda2)
 print(tienda3)
+
+carrito1 = Carrito()
+carrito1.agregarProducto(muneco)
+carrito1.agregarProducto(munecas)
+print(carrito1)
+carrito1.eliminarProducto(muneco)
+print(carrito1)
+
