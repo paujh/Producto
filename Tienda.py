c1648a1303131fc34b8f2929dcabbc7af9597839
paujh <48219253+paujh@users.noreply.github.com>
2019-05-13 19:57:55 -0500

    Update Tienda.py

--- Tienda.py
+++ Tienda.py
@@ -5,6 +5,7 @@ Paula Jorge Hinostrosa
 
 from Producto import *
 from Cliente import *
+from Carrito import *
 
 muneco = Producto("Muñeco Batman","LEGO",650)
 triciclo = Producto("Triciclo","Apache",1199)
@@ -20,12 +21,11 @@ rayo = Producto("Coche a Control Remoto Rayo McQueen Disney Collection","DISNEY 
 productos = [muneco,triciclo,munecas,camacoche,buz,elsa,vehiculo,figuras,casa,rayo]
 
 class Tienda:
-  
-  def __init__(self,nombre,ubicacion,cliente):
-    #Esta ocación, el constructo rebibirá 3 argumentos, donde 2 de ellos son objetos importados de otras clases ya creadas
-    self.nombre = nombre
-    self.ubicacion = ubicacion
-    self.cliente = cliente
+ 
+  def __init__(self,cliente):
+    self.cliente = cliente    
+    nombre = "Play Store"
+    ubicacion = "Avenida Cuauhtémoc 643, Narvarte Poniente, 03020 Ciudad de México, CDMX"
   
   def __str__(self): 
     #Este método es para que se visualicen los argumentos de la clase ya creada
@@ -33,4 +33,22 @@ class Tienda:
   
   def catalogoProductos:
     #Aquí haremos un método donde 
-    print(Los productos disponibles son: ",productos)
+    print("Los productos disponibles son: \n",str(productos))
+    eleccion = input("\n¿Desea agregar algún artículo a su carrito? Introduzca su respuesta (SI / NO): ")
+    if eleccion == "SI":
+      articulo = input("Introduzca el nombre del articulo que desea agregar al carrito: ")
+      for i in range (0,len(Tienda.productos)):
+				if articulo == Tienda.productos[i].nombre:
+					self.cliente.carrito.append(Tienda.productos[i])
+					print("Operación exitosa. Ha agregado  el artículo '",articulo,"' a su carrito.")
+    else: 
+      decision = input("¿Desea comprar los artículos de su carrito? (SI / NO): ")
+      if decision == "SI":
+        Cliente.comprar
+      else: 
+        pass
+    
+    def eliminar(self,producto):
+		  self.carrito.eliminarProducto(producto) 
+      
+    
