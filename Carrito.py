553250251c9c3da4f23b4cbade92bc6bb9de221e
paujh <48219253+paujh@users.noreply.github.com>
2019-05-13 19:19:51 -0500

    Update Carrito.py

--- Carrito.py
+++ Carrito.py
@@ -8,25 +8,26 @@ class Carrito:
       
   def agregarProducto(self,p):
     #Método para agregar un producto al carrito
-    carrito.append(p)
+    self.carrito.append(p)
 
   def eliminarProducto(self,r):
     #Método para eliminar un producto del carrito
-    carrito.remove(r)
+    self.carrito.remove(r)
 
   def __str__(self):
     #Método para desplegar los productos contenidos en el carrito
     "En tu carrito están estos productos: " + str(carrito)
   
-  def comprar(self):
-    #En este método haremos la suma de los precios de los productos del carrito, y veremos si el cliente puede o no comprar. 
-    suma = 0
-    n=len(carrito)
-    for i in range (n):
-      #Profesora, esta parte no estuve segura de como se debía de escribir para sumar los precios de los productos del carrito, por lo mismo, si pudiera corregir el código se lo agradecería mucho.
-      suma = math.floor(suma + (carrito.producto.precio[i]))
-    return suma 
-      #Por lo mismo, ya no supe cómo poner el condicional para ver si el cliente tiene el saldo suficiente
+ def comprar(self): #permite realizar compras (de manera primitiva)
+		Total = 0
+		for i in range(0,len(self.carrito)):
+			Total = Total + self.carrito[i].precio
+		return Total
+    if Total > Cliente.saldo:
+      print("No tienes saldo suficiente")
+    else:
+      print("Compra realizada")
+      
       
     
   
